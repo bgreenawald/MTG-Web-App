cd6f5ed86f2d0a7f99c671245ef636d317c30df7
Ben <bgreenawald@gmail.com>
2018-11-23 15:28:19 -0500

    Specified environments and enable linting

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Main runner function for the Flask app.
+"""
+
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
